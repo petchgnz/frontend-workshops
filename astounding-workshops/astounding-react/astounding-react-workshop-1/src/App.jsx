1589f572934687b9334250cd5cdb31a67c38bb68
Petchgnz <phetchgnz@gmail.com>
2025-02-24 02:39:58 +0700

    idk what am I doing

---
+++ astounding-workshops/astounding-react/astounding-react-workshop-1/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from "./components/Header.jsx"
+import Contents from "./components/Contents.jsx"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Contents />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ astounding-workshops/astounding-react/astounding-react-workshop-1/src/components/Contents.jsx
@@ -0,0 +1,12 @@
+function Contents() {
+  return (
+    <>
+
+      <div className="content-header">
+        <h1>A Massive Title That Uses Loads of Rooms</h1>
+      </div>
+    </>
+  )
+}
+
+export default Contents
