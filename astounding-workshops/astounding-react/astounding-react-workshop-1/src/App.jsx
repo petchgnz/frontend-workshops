8d26b32b598e4495310a42758ef1363b4494e6dd
Petchgnz <phetchgnz@gmail.com>
2025-02-24 02:47:57 +0700

    reset everything

--- astounding-workshops/astounding-react/astounding-react-workshop-1/src/App.jsx
+++ astounding-workshops/astounding-react/astounding-react-workshop-1/src/App.jsx
@@ -1,15 +1,9 @@
 import './App.css'
-import Header from "./components/Header.jsx"
-import Contents from "./components/Contents.jsx"
 
 function App() {
 
   return (
     <>
-      <Header />
-      <div className="container">
-        <Contents />
-      </div>
     </>
   )
 }
