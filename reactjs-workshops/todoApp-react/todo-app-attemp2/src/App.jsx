d3044c7ecc6ed0945b32bb026e40a4b2cefb6fae
Petchgnz <phetchgnz@gmail.com>
2025-04-01 04:50:24 +0700

    create new react project - todo_app with JavaScipt instead of TypeScript

---
+++ reactjs-workshops/todoApp-react/todo-app-attemp2/src/App.jsx
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <div className="bg-bg1 w-screen h-screen text-white flex flex-col justify-center items-center ">
+      <div className="text-2xl font-bold ">My Todos</div>
+
+      {/* Container */}
+      <div className="bg-bg2 m-5 p-[2%]">
+        {/* todo-input */}
+        <div className="min-w-[600px] h-auto grid gap-3 grid-cols-[1fr_1fr_80px] items-center border-b-2 border-white/25 pb-5 mb-5">
+          {/* todo-input-items */}
+          <div className="flex flex-col">
+            <label className="font-bold mb-2 text-lg">Task</label>
+            <input className="outline-none p-2 bg-white/10 text-white" type="text" placeholder="What's the task title?" />
+          </div>
+
+          <div className="flex flex-col">
+            <label className="font-bold mb-2 text-lg">Description</label>
+            <input className="outline-none p-2 bg-white/10 text-white" type="text" placeholder="What's the task description?" />
+          </div>
+
+          <div className="flex justify-center mt-8">
+			 <button className="w-[60px] p-2 bg-emr cursor-pointer transition-all ease-in-out hover:bg-emerald-500 rounded-xs" type="button">Add</button>
+		  </div>
+        </div>
+
+        {/* todo-btn-area */}
+        <div className="">
+          <button className="">Todo</button>
+          <button className="">Completed</button>
+        </div>
+      </div>
+    </div>
+  );
+}
