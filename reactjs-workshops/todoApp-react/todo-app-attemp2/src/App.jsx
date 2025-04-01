c5c4c2cee3aa9cb40697d9baee43754ce865ca66
petchgnz <phetchgnz@gmail.com>
2025-04-01 17:32:44 +0700

    Doing js a lil bit

--- reactjs-workshops/todoApp-react/todo-app-attemp2/src/App.jsx
+++ reactjs-workshops/todoApp-react/todo-app-attemp2/src/App.jsx
@@ -4,6 +4,27 @@ import { FaCheck } from "react-icons/fa6";
 
 export default function App() {
   const [isCompletedScreen, setIsCompletedScreen] = useState(false);
+  const [allTodos, setAllTodos] = useState([]);
+  const [newTitle, setNewTitle] = useState('');
+  const [newDescription, setNewDescription] = useState('');
+
+  const handleAddTodo = () => {
+    if (!newTitle.trim() || !newDescription.trim()) {
+      alert("Task title and description cannot be empty!")
+      return;
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      title: newTitle,
+      description: newDescription,
+      isCompletedScreen: false,
+    };
+
+    setAllTodos([...allTodos, newTodo])
+    setNewTitle("")
+    setNewDescription("")
+  }
 
   return (
     <div className="bg-bg1 w-screen h-screen text-white flex flex-col justify-center items-center shadow-2xl">
@@ -15,17 +36,35 @@ export default function App() {
         <div className="min-w-[600px] h-auto grid gap-3 grid-cols-[1fr_1fr_80px] items-center border-b-2 border-white/25 pb-5 mb-5">
           {/* todo-input-items */}
           <div className="flex flex-col">
-            <label className="font-bold mb-2 text-lg">Task</label>
-            <input className="outline-none p-2 bg-white/10 text-white" type="text" placeholder="What's the task title?" />
+            <label className="font-bold mb-2 text-lg">Title</label>
+            <input
+              className="outline-none p-2 bg-white/10 text-white"
+              placeholder="What's the task title?"
+              onChange={(e) => setNewTitle(e.target.value)}
+              value={newTitle}
+              type="text"
+
+            />
           </div>
 
           <div className="flex flex-col">
             <label className="font-bold mb-2 text-lg">Description</label>
-            <input className="outline-none p-2 bg-white/10 text-white" type="text" placeholder="What's the task description?" />
+            <input
+              className="outline-none p-2 bg-white/10 text-white"
+              placeholder="What's the task description?"
+              onChange={(e) => setNewDescription(e.target.value)}
+              value={newDescription}
+              type="text"
+            />
           </div>
 
           <div className="flex justify-center mt-8">
-            <button className="w-[60px] p-2 bg-emr cursor-pointer transition-all ease-in-out hover:bg-emerald-500 rounded-xs" type="button">Add</button>
+            <button
+              className="w-[60px] p-2 bg-emr cursor-pointer transition-all ease-in-out hover:bg-emerald-500 rounded-xs" type="button"
+              onClick={handleAddTodo}
+            >
+              Add
+            </button>
           </div>
         </div>
 
@@ -33,7 +72,6 @@ export default function App() {
         <div className="">
           <button className={`cursor-pointer w-[60px] p-2 ${!isCompletedScreen ? "bg-emr" : "bg-[#686868]"}`} onClick={() => setIsCompletedScreen(false)}>Todo</button>
           <button className={`cursor-pointer p-2 ${isCompletedScreen ? "bg-emr" : "bg-[#686868]"}`} onClick={() => setIsCompletedScreen(true)}>Completed</button>
-          {console.log(isCompletedScreen)}
         </div>
 
         {/* todo item*/}
